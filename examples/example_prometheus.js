17a6cc38510ab1d9b6de4218f593d3d23b921380
Eundoo Song <eundoo.song@gmail.com>
2018-06-11 23:47:45 +0900

    Add examples

---
+++ examples/example_prometheus.js
@@ -0,0 +1,51 @@
+import { initTracer } from '../src';
+import { PrometheusMetricsFactory } from '../src';
+import express from 'express';
+import { register as globalRegistry } from 'prom-client';
+
+console.log(PrometheusMetricsFactory);
+
+const app = express();
+
+var config = {
+  serviceName: 'prometheus_test',
+  sampler: {
+    type: 'remote',
+    param: 1,
+    refreshIntervalMs: 10000,
+  },
+  reporter: {
+    logSpans: true,
+  },
+};
+
+//globalRegistry.setDefaultLabels({ service_name: config.serviceName });
+var metrics = new PrometheusMetricsFactory(config.serviceName);
+var options = {
+  tags: {
+    version: '0.0.1',
+  },
+  metrics: metrics,
+  logger: console,
+};
+
+var tracer = initTracer(config, options);
+
+var cnt = 1;
+app.get('/log', (req, res) => {
+  var span = tracer.startSpan('test:' + cnt);
+  span.setTag('key', 'value');
+
+  res.end('log');
+
+  span.finish();
+  cnt = cnt + 1;
+});
+
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', globalRegistry.contentType);
+  res.end(globalRegistry.metrics());
+});
+
+console.log('Server listening to 3000');
+app.listen(3000);
